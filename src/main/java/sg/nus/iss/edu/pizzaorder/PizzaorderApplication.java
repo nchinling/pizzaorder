bdf0366246582dd8b87229bde60cf4d7db81c432
nchinling <nchinling@gmail.com>
2023-03-08 20:49:27 +0800

    first commit

---
+++ src/main/java/sg/nus/iss/edu/pizzaorder/PizzaorderApplication.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.edu.pizzaorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaorderApplication.class, args);
+	}
+
+}
---
+++ src/main/java/sg/nus/iss/edu/pizzaorder/controller/PizzaOrderController.java
@@ -0,0 +1,29 @@
+package sg.nus.iss.edu.pizzaorder.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
+// import sg.nus.iss.edu.pizzaorder.model.Order;
+// import sg.nus.iss.edu.pizzaorder.service.OrderService;
+
+
+@Controller
+@RequestMapping(path="/")
+public class PizzaOrderController {
+    
+    @GetMapping
+    public String getMain(Model m){
+        // m.addAttribute("contact", new Contact());
+        return "index";
+    }
+}
